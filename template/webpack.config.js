563cfb01363c584a6e44bff301df8c91e5abd444
GLEB BAYESHKO <bayeshko_gleb@mail.ru>
2020-10-10 21:41:33 +0300

    feat: implement layout of empty cart; implement full responsive design

---
+++ template/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+    const isProduction = options.mode === 'production';
+
+    const config = {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'source-map',
+        watch: !isProduction,
+        context: path.resolve(__dirname),
+        entry: ['./src/index.js', './src/sass/main.scss'],
+        output: {
+            path: path.join(__dirname, './build'),
+            filename: 'script.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|woff)$/i,
+                    loader: 'file-loader',
+                    options: {
+                      outputPath: 'assets',
+                    },
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                    options: {
+                        attributes: {
+                            root: '.'
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'index.html',
+                filename: "index.html",
+            }),
+            new HtmlWebpackPlugin({
+                template: 'cart-full.html',
+                filename: "cart-full.html",
+            }),
+            new HtmlWebpackPlugin({
+                template: 'cart-empty.html',
+                filename: "cart-empty.html",
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+            })
+        ]
+    }
+
+    return config;
+}
