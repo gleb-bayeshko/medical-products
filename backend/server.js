bb17c5a016c3c3626228a48d56e526933e9c875b
GLEB BAYESHKO <bayeshko_gleb@mail.ru>
2020-11-10 19:38:50 +0300

    change port settings

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../app/build/index.html`));
 });
 
-app.listen(config.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`Server is up on ${config.PORT} port` );
 });
